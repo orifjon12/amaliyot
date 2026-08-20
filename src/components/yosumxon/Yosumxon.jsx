c8bcc9258c63b271cb40149ab8af224bfa5a17e5
Yosirxon <mutalliyevyosirhon@gmail.com>
2026-08-20 15:30:56 +0500

    Yosirxon finished

--- src/components/yosumxon/Yosumxon.jsx
+++ src/components/yosumxon/Yosumxon.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import rox  from '../../assets/rox.png'
+import './Yosumxon.css'
 const Yosumxon = () => {
   return (
     <div>
-      
+      <section className='section'>
+        <div>
+          <h3 className='h1'>Check out your monthly <br /> summary with a glance.</h3>
+          <p className='p'>Vivamus facilisis, neque sit amet venenatis euismod, <br /> neque viverra velit, sit amet vehicula sapien elit eget tortor.</p>
+        </div>
+        <div>
+          <img src={rox} alt="" />
+        </div>
+      </section>
     </div>
   )
 }
