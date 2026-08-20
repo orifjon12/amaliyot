6d5c12fb74e9a25ca665432e9e0f6ceee80225af
Muhammadali Dadaxanov <dadaxanovmuhammadali17@gmail.com>
2026-08-20 15:17:40 +0500

    muhammadali

--- src/components/muxammadAli/MuxammadAli.jsx
+++ src/components/muxammadAli/MuxammadAli.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./MuxammadAli.css"
+import img from "./imager.svg"
+
+import { useTranslation } from 'react-i18next'
 
 const MuxammadAli = () => {
+  const [t,i18n] = useTranslation()
   return (
-    <div>
-      
+    <div className='muhammadali container'>
+        <div className="muhammadali-texts">
+                <p className="muhammadali-text-1">{t("muhammadali.bigText")}</p>
+                <p className="muhammadali-text-2">{t("muhammadali.smallText")}</p>
+            </div>
+            <div>
+                <img src={img} alt="" />
+            </div>
     </div>
   )
 }
